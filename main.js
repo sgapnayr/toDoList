920a686c297b354af48f365ce60754d6ed8d6c55
“Ryan” <pagsmanagement@gmail.com>
2022-05-27 13:34:28 -0500

    save

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
-const input = document.querySelector('input')
-const toDoInput = document.querySelector('.todo-input')
+const input = document.querySelector('.todo-input')
 const toDoButton = document.querySelector('.todo-button')
 const toDoList = document.querySelector('.todo-list')
 
@@ -9,34 +7,62 @@ toDoButton.addEventListener('click', addToDo)
 function addToDo(event) {
     event.preventDefault()
     if (!input.value) { return } else {
+        const toDoDiv = document.createElement('div')
+        toDoDiv.classList.add('toDoDiv')
 
-        const todoDiv = document.createElement('div')
-        todoDiv.classList.add('todo')
-
-        const newTodo = document.createElement('li')
-        newTodo.innerText = input.value
+        const toDoItem = document.createElement('li')
+        toDoItem.innerText = input.value
         input.value = ''
-        newTodo.classList.add('todo-item')
-        todoDiv.appendChild(newTodo)
+        toDoItem.classList.add('toDoItem')
+        toDoDiv.appendChild(toDoItem)
+
+        toDoItem.addEventListener('dblclick', editItem)
+
+        function editItem(event) {
+            let item = event.target.innerText
+            let itemInput = document.createElement('input')
+            itemInput.autofocus
+            itemInput.type = 'text'
+            itemInput.value = item
+            itemInput.classList.add('editItem')
+            itemInput.addEventListener('keypress', saveItem)
+            itemInput.addEventListener('click', saveItem)
+            event.target.parentNode.prepend(itemInput)
+            event.target.remove()
+            itemInput.select()
+        }
+
+        function saveItem(event) {
+            let inputValue = event.target.innerText
+            if (event.target.value.length > 0 && (event.keyCode === 13 || event.type === 'click')) {
+                let li = document.createElement('li')
+                li.addEventListener('click', toggleDone)
+                li.addEventListener('dblclick', editItem)
+                li.innerText = event.target.value
+                event.target.parentNode.prepend(li)
+                event.target.remove()``
+            }
+        }
 
         const completedButton = document.createElement('button')
         completedButton.innerHTML = 'C'
-        completedButton.classList.add('complete-btn')
-        todoDiv.appendChild(completedButton)
+        completedButton.classList.add('complete-button')
+        toDoDiv.appendChild(completedButton)
 
-        const trashButton = document.createElement('button')
-        trashButton.innerHTML = 'X'
-        trashButton.classList.add('trash-btn')
-        todoDiv.appendChild(trashButton)
+        const removeButton = document.createElement('button')
+        removeButton.innerHTML = 'X'
+        removeButton.classList.add('remove-button')
+        toDoDiv.appendChild(removeButton)
 
-        toDoList.appendChild(todoDiv)
+        toDoList.appendChild(toDoDiv)
 
         completedButton.addEventListener('click', () => {
-            newTodo.style.textDecoration = 'line-through'
+            toDoItem.style.textDecoration = 'line-through'
         })
 
-        trashButton.addEventListener('click', () => {
-            toDoList.removeChild(todoDiv)
+        removeButton.addEventListener('click', () => {
+            toDoList.removeChild(toDoDiv)
         })
     }
 }
+
